be76e16a4352184ecb68ec23d9f506eae9d4db17
Bảo <55090875+Corey-Stowe@users.noreply.github.com>
2020-12-06 15:29:32 +0700

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -8,13 +8,12 @@ sudo apt install nodejs -y
 sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev -y
 sleep 3
 echo "downloading data..."
-wget https://www.dropbox.com/s/nkz6qak4i8uliec/c3c.zip?dl=1
-mv c3c.zip?dl=1 c3c.zip
-unzip c3c.zip
+git clone http://github.com/lequanglam/c3c
+cd c3c
+npm i
 echo "Đã hoàn tất cài bot"
 echo " "
 echo "để config file vui lòng qua VNC để chỉnh sửa"
 echo " "
 #địt mẹ thằng nào làm tools .php mà không viết soucre người làm scripts thì pay acc ok
-cd c3c
 npm start
